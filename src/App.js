055357957f649b33169c7c4163ff926b86746f6c
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2017-12-13 15:25:17 -0500

    started login page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Landing from './components/landing';
+import Login from './components/login';
 
 class App extends Component {
   render() {
     return (
       // <div className="App">
-        <Landing />
+        <Login />
+        // <Landing />
       // </div>
     );
   }
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,2 +1,53 @@
 import React from 'react';
 import './login.css';
+import './landing.css';
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      login: false
+    };
+  }
+
+  switcher(){
+    if(this.props.login){
+      return(
+        <div>
+          <input type="text" placeholder="Your Email" />
+          <input type="text" placeholder="Your Email" />
+          <input type="text" placeholder="Your Email" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input type="text" placeholder="Your Email" />
+          <input type="text" placeholder="Your Email" />
+          <input type="text" placeholder="Your Email" />
+        </div>
+      );
+    }
+  }
+
+
+  render(){
+    return (
+      <div className="login">
+        <div className="login-content">
+          <div className="top-bar">
+            <div>Reactor</div>
+          </div>
+
+          <div className="login-form">
+            <div className="switcher">
+            Login Sign Up
+            </div>
+            { switcher() }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
